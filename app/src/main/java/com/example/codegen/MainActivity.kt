394c0f492093da213a19defb97035b3be4079b12
jakobkwiatkowski <244887@student.pwr.edu.pl>
2022-05-19 16:28:00 +0200

    dodane komentarze

--- app/src/main/java/com/example/codegen/MainActivity.kt
+++ app/src/main/java/com/example/codegen/MainActivity.kt
@@ -11,7 +11,9 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://besafe-ecb67-default-rtdb.europe-west1.firebasedatabase.app")
+    //utworzenie zmiennej databaseReference, łączącej projekt z realtime database w Firebase
+    val databaseReference: DatabaseReference = FirebaseDatabase.getInstance()
+        .getReferenceFromUrl("https://besafe-ecb67-default-rtdb.europe-west1.firebasedatabase.app")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,29 +21,32 @@ class MainActivity : AppCompatActivity() {
 
         var generateButton = findViewById<Button>(R.id.GenerateButton)
 
-
+        // wywołanie funkcji generującej kody aktywacyjne po kliknięciu przycisku "generuj"
         generateButton.setOnClickListener {
             generateCodes()
         }
     }
 
+    //funkcja generująca zadaną przez użytkownika liczbę kodów aktywacyjnych oraz umieszczająca je w bazie danych
     private fun generateCodes() {
+        //pobranie wartości wprowadzonych przez użytkownika
         var numofcodes = findViewById<EditText>(R.id.NumberOfCodes)
         var numbertxt = numofcodes.getText().toString()
         var number = Integer.parseInt(numbertxt)
 
-
-        for(i in 1 .. number){
+        //generowanie określonej liczby kodów w pętli for, przesłanie ich do bazy danych
+        for (i in 1..number) {
+            // generowanie 128-bitowego ciągu znaków UUID
             val newKEY = UUID.randomUUID().toString()
+            // skrócenie kodu o 20 pierwszych znaków
             val key = newKEY.drop(20)
 
+            //przesłanie kodu do bazy danych
             databaseReference.child("ActivationKeys").child(key).child("Key").setValue(key)
         }
 
+        // wyświetlenie informacji o pomyślnym wygenerowaniu kodu, zamknięcie aplikacji.
         Toast.makeText(this, "Kody zostały pomyślnie wygenerowane", Toast.LENGTH_SHORT).show()
         finish()
-
     }
-
-
 }
